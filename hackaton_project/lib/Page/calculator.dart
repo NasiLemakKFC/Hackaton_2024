49ee9b41aa1a2b8c47ff992159cddca7c2741192
dany4240 <asyrafdanial318@gmail.com>
2024-10-30 00:39:59 +0800

    ada tambah sikit

---
+++ hackaton_project/lib/Page/calculator.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:hackaton_project/Page/chatpage.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
